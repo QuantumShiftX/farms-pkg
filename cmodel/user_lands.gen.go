72913c14fac74ed3d22504b5ee4759740dc02b43
o1was <guanlinhai520@gmail.com>
2025-03-03 20:34:55 +0800

    ✨ Feat(种植记录id): 种植记录ID
    种植记录ID

--- cmodel/user_lands.gen.go
+++ cmodel/user_lands.gen.go
@@ -13,6 +13,7 @@ type UserLand struct {
 	LandID   int64     `gorm:"column:land_id;comment:土地ID" json:"land_id"`
 	UserID   int64     `gorm:"column:user_id;comment:用户ID" json:"user_id"`
 	FarmID   int64     `gorm:"column:farm_id;comment:农场ID" json:"farm_id"`
+	PlantID  int64     `gorm:"column:plant_id;comment:种植记录ID" json:"plant_id"`
 	Position int64     `gorm:"column:position;comment:土地位置编号（1-20）" json:"position"`
 	Status   int64     `gorm:"column:status;default:1;comment:土地状态：1-未开垦，2-已开垦未种植，3-已种植" json:"status"`
 	//

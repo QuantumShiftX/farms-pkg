a4a1bb9182b9a5804a7d7b8204b3eac40508a366
o1was <guanlinhai520@gmail.com>
2025-03-04 23:07:12 +0800

    ✨ Feat(farms): farms
    farms

--- constants/farms.go
+++ constants/farms.go
@@ -1,9 +1,5 @@
 package constants
 
-import (
-	"github.com/QuantumShiftX/farms-proto/proto-gen-go/user/v1"
-)
-
 // StoreProductType 存储商品类型
 type StoreProductType int64
 
@@ -37,14 +33,6 @@ const (
 const (
 	DefaultGrowthTime = 864000 // 默认10天（秒）
 )
-
-// OperationTypeMap 将protobuf枚举映射到数据库操作的int64
-var OperationTypeMap = map[v1.FarmOperationType]int64{
-	v1.FarmOperationType_PLANTING:    1,
-	v1.FarmOperationType_WATERING:    2,
-	v1.FarmOperationType_FERTILIZING: 3,
-	v1.FarmOperationType_HARVESTING:  4,
-}
 
 // FarmOperationType 操作类型
 type FarmOperationType int64

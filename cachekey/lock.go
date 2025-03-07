6e970c9aa5725b848558f633ceebbe7060a8ecc8
o1was <guanlinhai520@gmail.com>
2025-03-07 20:48:45 +0800

    ✨ Feat(lock): lock
    lock

--- cachekey/lock.go
+++ cachekey/lock.go
@@ -11,3 +11,8 @@ const (
 	// 	LockUserFarmsOpsKey 用户操作农场锁key {user_id} {ops_farm_id}
 	LockUserFarmsOpsKey = "{lock}:user_%d_ops_farms_%d"
 )
+
+const (
+	// 	LockUserFarmsOpsKey 用户操作农场锁key {user_id} {product_id}
+	LockUserOpsStoreProductKey = "{lock}:user_%d_ops_product_%d"
+)

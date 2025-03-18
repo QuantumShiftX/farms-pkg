62e9f54ba67d506529d24fb848708da0511136b4
o1was <guanlinhai520@gmail.com>
2025-03-18 11:34:10 +0800

    ✨ Feat(dingyi meeiju): dingyi meeiju

--- constants/status.go
+++ constants/status.go
@@ -25,3 +25,29 @@ func (t StatusEnabledType) Default() StatusEnabledType {
 	}
 	return t
 }
+
+// 定义是否可操作的枚举
+type OperableEnum int
+
+const (
+	// 可操作
+	Operable OperableEnum = 1
+
+	// 不可操作
+	UnOperable OperableEnum = 2
+)
+
+// 转换枚举为字符串的映射（可选）
+var OperableEnumStrings = map[OperableEnum]string{
+	Operable:   "可操作",
+	UnOperable: "不可操作",
+}
+
+func (o OperableEnum) Int64() int64 {
+	return int64(o)
+}
+
+// 示例函数：输出是否可操作
+func (o OperableEnum) String() string {
+	return OperableEnumStrings[o]
+}

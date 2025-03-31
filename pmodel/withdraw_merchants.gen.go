98cd2ede7f2baad61577f478e632f9efcf0aee89
o1was <guanlinhai520@gmail.com>
2025-03-31 14:28:19 +0800

    ✨ Feat(model): model
    model

---
+++ pmodel/withdraw_merchants.gen.go
@@ -0,0 +1,34 @@
+package pmodel
+
+import "github.com/QuantumShiftX/golib/stores/gormx"
+
+// 三方代付平台表
+const TableNameWithdrawMerchant = "withdraw_merchants"
+
+// WithdrawMerchant mapped from table <withdraw_merchants>
+type WithdrawMerchant struct {
+	ID                    int64  `gorm:"column:id;primaryKey;autoIncrement:true;comment:自增主键" json:"id"`                                              // 自增主键
+	Name                  string `gorm:"column:name;type:varchar(255);not null;comment:三方代付平台名称" json:"name"`                                         // 三方代付平台名称
+	CurrencyCode          string `gorm:"column:currency_code;type:varchar(255);not null;comment:币种编码" json:"currency_code"`                           // 币种编码
+	PaymentConfigID       int32  `gorm:"column:payment_config_id;type:integer;not null;comment:三方支付/代付id" json:"payment_config_id"`                   // 三方支付/代付id
+	MerchantNo            string `gorm:"column:merchant_no;type:varchar(255);not null;comment:商户号" json:"merchant_no"`                                // 商户号
+	Key                   string `gorm:"column:key;type:varchar(255);not null;comment:密钥" json:"key"`                                                 // 密钥
+	ExternalInfo          string `gorm:"column:external_info;type:text;not null;comment:扩展信息" json:"external_info"`                                   // 扩展信息
+	WithdrawOrderPayURL   string `gorm:"column:withdraw_order_pay_url;type:varchar(255);not null;comment:下单地址(提现下单地址)" json:"withdraw_order_pay_url"` // 下单地址(提现下单地址)
+	WithdrawOrderQueryURL string `gorm:"column:withdraw_order_query_url;type:varchar(255);not null;comment:订单查询地址" json:"withdraw_order_query_url"`   // 订单查询地址
+	BalanceQueryURL       string `gorm:"column:balance_query_url;type:varchar(255);not null;comment:余额查询地址" json:"balance_query_url"`                 // 余额查询地址
+	SuccessTag            string `gorm:"column:success_tag;type:varchar(255);default:success;comment:提现成功标识(回调成功标识)" json:"success_tag"`              // 提现成功标识(回调成功标识)
+	CallbackAddress       string `gorm:"column:callback_address;type:varchar(255);comment:回调IP地址(多个用逗号分隔)" json:"callback_address"`                   // 回调IP地址(多个用逗号分隔)
+	WithdrawTypeIDs       []byte `gorm:"column:withdraw_type_ids;type:jsonb;comment:提现方式Id's" json:"withdraw_type_ids"`                               // 提现方式Id's
+	LimitAmountLower      int64  `gorm:"column:limit_amount_lower;not null;comment:最低提现金额" json:"limit_amount_lower"`                                 // 最低提现金额
+	LimitAmountUpper      int64  `gorm:"column:limit_amount_upper;not null;comment:最高提现金额" json:"limit_amount_upper"`                                 // 最高提现金额
+	Status                int8   `gorm:"column:status;type:smallint;not null;comment:状态(1:启用,2:禁用)" json:"status"`                                    // 状态(1:启用,2:禁用)
+	Remark                string `gorm:"column:remark;type:varchar(1000);not null;comment:备注" json:"remark"`                                          // 备注
+	gormx.OperationBaseModel
+	gormx.Model
+}
+
+// TableName WithdrawMerchant's table name
+func (*WithdrawMerchant) TableName() string {
+	return TableNameWithdrawMerchant
+}
---
+++ pmodel/withdraw_payment_records.gen.go
@@ -0,0 +1,29 @@
+package pmodel
+
+import "github.com/QuantumShiftX/golib/stores/gormx"
+
+// 代付记录表
+const TableNameWithdrawPaymentRecord = "withdraw_payment_records"
+
+// WithdrawPaymentRecord mapped from table <withdraw_payment_records>
+type WithdrawPaymentRecord struct {
+	ID                 int64  `gorm:"column:id;primaryKey;autoIncrement:true;comment:自增主键" json:"id"`                                                                                              // 自增主键
+	OrderNumber        string `gorm:"column:order_number;type:varchar(255);not null;index:idx_withdraw_payment_records_order_number,priority:1;comment:订单号" json:"order_number"`                   // 订单号
+	ThirdOrderNumber   string `gorm:"column:third_order_number;type:varchar(255);comment:三方订单号" json:"third_order_number"`                                                                         // 三方订单号
+	WithdrawMerchantID int64  `gorm:"column:withdraw_merchant_id;not null;comment:三方代付商户ID" json:"withdraw_merchant_id"`                                                                           // 三方代付商户ID
+	WithdrawTypeID     int64  `gorm:"column:withdraw_type_id;not null;comment:提现方式" json:"withdraw_type_id"`                                                                                       // 提现方式
+	Status             int32  `gorm:"column:status;not null;index:idx_withdraw_payment_records_user_id_status,priority:2;comment:代付状态 1-订单提交成功 2-付款失败 3-已付款 4-已强制出款 5-已强制取消 6-强制失败" json:"status"` // 代付状态 1-订单提交成功 2-付款失败 3-已付款 4-已强制出款 5-已强制取消 6-强制失败
+	Remark             string `gorm:"column:remark;type:varchar(255);comment:备注" json:"remark"`                                                                                                    // 备注
+	Req                string `gorm:"column:req;type:text;comment:请求参数" json:"req"`                                                                                                                // 请求参数
+	Resp               string `gorm:"column:resp;type:text;comment:响应参数" json:"resp"`                                                                                                              // 响应参数
+	UserID             int64  `gorm:"column:user_id;index:idx_withdraw_payment_records_user_id_status,priority:1;comment:用户ID" json:"user_id"`                                                     // 用户ID
+	Amount             int64  `gorm:"column:amount;comment:提现金额" json:"amount"`                                                                                                                    // 提现金额
+	CurrencyCode       string `gorm:"column:currency_code;type:varchar(255);comment:币种" json:"currency_code"`                                                                                      // 币种
+	gormx.OperationBaseModel
+	gormx.Model
+}
+
+// TableName WithdrawPaymentRecord's table name
+func (*WithdrawPaymentRecord) TableName() string {
+	return TableNameWithdrawPaymentRecord
+}

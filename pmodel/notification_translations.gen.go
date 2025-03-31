f9a99289234c01a5d8620d69ca97c90ce19870a7
o1was <guanlinhai520@gmail.com>
2025-03-31 18:16:38 +0800

    ✨ Feat(zzz): zzz
    zzz

--- pmodel/notification_translations.gen.go
+++ pmodel/notification_translations.gen.go
@@ -11,6 +11,7 @@ type NotificationTranslation struct {
 	LanguageCode string `gorm:"column:language_code;size:10;not null" json:"language_code"` // 语言编码，如 'zh', 'en'
 	Title        string `gorm:"column:title;not null" json:"title"`                         // 通知标题
 	Content      string `gorm:"column:content;not null" json:"content"`                     // 通知内容
+	Remarks      string `gorm:"column:remarks" json:"remarks"`                              // 备注，描述模板的用途或说明
 	//
 	gormx.OperationBaseModel
 	gormx.Model

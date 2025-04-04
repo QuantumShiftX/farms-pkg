25cbd6067e8b0935e133c42497fb70bbf32a17fe
o1was <guanlinhai520@gmail.com>
2025-04-04 18:50:08 +0800

    ✨ Feat(pkg): pkg
    pkg

--- pmodel/faq_categories.gen.go
+++ pmodel/faq_categories.gen.go
@@ -9,11 +9,11 @@ const TableNameFaqCategory = "faq_categories"
 
 // FAQ分类表模型
 type FaqCategory struct {
-	ID           int64          `gorm:"primaryKey;autoIncrement;column:id;type:bigint"`
-	CategoryName datatypes.JSON `gorm:"column:category_name;type:jsonb;not null;default:'{\"en\":\"\"}'"`
-	Sort         int64          `gorm:"column:sort;type:bigint;not null"`
-	Status       int8           `gorm:"column:status;type:smallint;not null;default:1"`
-	Remarks      string         `gorm:"column:remarks;type:text"`
+	ID           int64          `gorm:"primaryKey;autoIncrement;column:id;type:bigint" json:"id"`
+	CategoryName datatypes.JSON `gorm:"column:category_name;type:jsonb;not null;default:'{\"en\":\"\"}'" json:"category_name"`
+	Sort         int64          `gorm:"column:sort;type:bigint;not null" json:"sort"`
+	Status       int8           `gorm:"column:status;type:smallint;not null;default:1" json:"status"`
+	Remarks      string         `gorm:"column:remarks;type:text" json:"remarks"`
 	//
 	gormx.OperationBaseModel
 	gormx.Model
--- pmodel/faq_items.gen.go
+++ pmodel/faq_items.gen.go
@@ -9,12 +9,12 @@ const TableNameFaqItem = "faq_items"
 
 // FAQ条目表模型
 type FaqItem struct {
-	ID         int64          `gorm:"primaryKey;autoIncrement;column:id;type:bigint"`
-	CategoryID int64          `gorm:"column:category_id;type:bigint;not null"`
-	Question   datatypes.JSON `gorm:"column:question;type:jsonb;not null;default:'{\"en\":\"\"}'"`
-	Answer     datatypes.JSON `gorm:"column:answer;type:jsonb;not null;default:'{\"en\":\"\"}'"`
-	Status     int8           `gorm:"column:status;type:smallint;not null;default:1"`
-	Remarks    string         `gorm:"column:remarks;type:text"`
+	ID         int64          `gorm:"primaryKey;autoIncrement;column:id;type:bigint" json:"id"`
+	CategoryID int64          `gorm:"column:category_id;type:bigint;not null" json:"category_id"`
+	Question   datatypes.JSON `gorm:"column:question;type:jsonb;not null;default:'{\"en\":\"\"}'" json:"question"`
+	Answer     datatypes.JSON `gorm:"column:answer;type:jsonb;not null;default:'{\"en\":\"\"}'" json:"answer"`
+	Status     int8           `gorm:"column:status;type:smallint;not null;default:1" json:"status"`
+	Remarks    string         `gorm:"column:remarks;type:text" json:"remarks"`
 	//
 	gormx.OperationBaseModel
 	gormx.Model

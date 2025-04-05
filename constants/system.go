b024f3d77bca3ce2078d1206d51aa1b9c13c0ad2
o1was <guanlinhai520@gmail.com>
2025-04-05 14:39:32 +0800

    ✨ Feat(充值退款): 充值退款
    充值退款

--- constants/system.go
+++ constants/system.go
@@ -6,6 +6,7 @@ const (
 	BasicSettingsCategoryConf    SettingsConfCategory = "basic_settings" // 基础配置
 	AgentRebateRulesCategoryConf SettingsConfCategory = "rebate_rules"   // 代理规则
 	AgreementCategoryConf        SettingsConfCategory = "agreement"      // 协议
+	FundsPolicyCategoryConf      SettingsConfCategory = "funds_policy"   // 充值提款
 )
 
 func (t SettingsConfCategory) Str() string {
@@ -20,6 +21,8 @@ const (
 	AgreementUserAgreementKeyConf   SettingsConfKey = "user_agreement"
 	AgreementPrivacyPolicyKeyConf   SettingsConfKey = "privacy_policy"
 	AgreementCustomerServiceKeyConf SettingsConfKey = "customer_service"
+	FundsDepositPolicyKeyConf       SettingsConfKey = "deposit_policy"
+	FundsWithdrawalPolicyKeyConf    SettingsConfKey = "withdrawal_policy"
 )
 
 func (t SettingsConfKey) Str() string {

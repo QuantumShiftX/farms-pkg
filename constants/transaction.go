4fc6cb5d75c07667d51082f7e3a3db6ce97f10f2
o1was <guanlinhai520@gmail.com>
2025-04-09 15:23:42 +0800

    ✨ Feat(zzzz): zzzz
    zzzz

--- constants/transaction.go
+++ constants/transaction.go
@@ -4,12 +4,15 @@ package constants
 type TxnCategoryType int
 
 const (
-	TxnDeposit          TxnCategoryType = iota + 1 // 1: 存款
-	TxnWithdraw                                    // 2: 提款
-	TxnPayment                                     // 3: 收款
-	TxnPurchase                                    // 4: 购买
-	TxnCurrencyExchange                            // 5: 换汇
-	TxnRebate                                      // 6: 返佣
+	TxnDeposit          TxnCategoryType = iota + 1 // 1: 存款 (充值)
+	TxnWithdraw                                    // 2: 提款 (提现)
+	TxnPurchase                                    // 3: 购买
+	TxnCurrencyExchange                            // 4: 换汇
+	TxnRebate                                      // 5: 返佣
+	TxnReward                                      // 6: 奖励
+	TxnHarvest                                     // 7: 收获
+	TxnDepositPromotion                            // 8: 充值奖励
+	TxnFundCorrection                              // 9: 资金修正
 )
 
 // 定义枚举类型的字符串映射
@@ -19,14 +22,20 @@ func (o TxnCategoryType) String() string {
 		return "存款"
 	case TxnWithdraw:
 		return "提款"
-	case TxnPayment:
-		return "收款"
 	case TxnPurchase:
 		return "购买"
 	case TxnCurrencyExchange:
 		return "换汇"
 	case TxnRebate:
 		return "返佣"
+	case TxnReward:
+		return "奖励"
+	case TxnHarvest:
+		return "收获"
+	case TxnDepositPromotion:
+		return "充值奖励"
+	case TxnFundCorrection:
+		return "资金修正"
 	default:
 		return "未知操作"
 	}
@@ -38,8 +47,9 @@ type TxnSubCategoryType int
 const (
 	TxnSubDeposit          TxnSubCategoryType = iota + 1 // 1: 存款
 	TxnSubWithdraw                                       // 2: 提款
-	TxnSubPayment                                        // 3: 收款
-	TxnSubPurchase                                       // 4: 购买
-	TxnSubCurrencyExchange                               // 5: 换汇
-	TxnSubRebate                                         // 6: 返佣
+	TxnSubPurchase                                       // 3: 购买
+	TxnSubCurrencyExchange                               // 4: 换汇
+	TxnSubRebate                                         // 5: 返佣
+	TxnSubReward                                         // 6: 返佣
+	TxnSubHarvest                                        // 7: 返佣
 )

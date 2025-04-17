ea8fd2490d54e86370e1d04a001508151fffd7ed
o1was <guanlinhai520@gmail.com>
2025-04-17 17:45:10 +0800

    ✨ Feat(zzz): zzz
    zzz

--- cmodel/user_infos.gen.go
+++ cmodel/user_infos.gen.go
@@ -4,16 +4,16 @@ const TableNameUserInfo = "user_infos"
 
 // UserInfo 用户信息表
 type UserInfo struct {
-	ID         string `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
-	UserID     int64  `gorm:"column:user_id;uniqueIndex" json:"user_id"`
-	Username   string `gorm:"column:username" json:"username"`
-	AvatarURL  string `gorm:"column:avatar_url" json:"avatar_url"`
-	VIPLevelID int64  `gorm:"column:vip_level_id;default:1" json:"vip_level_id"`
-	FarmID     int64  `gorm:"column:farm_id" json:"farm_id"`
-	LandID     int64  `gorm:"column:land_id" json:"land_id"`
-	Coin       int64  `gorm:"column:coin;default:0" json:"coin"`
-	Status     int64  `gorm:"column:status;default:1" json:"status"`
-	UpdatedAt  int64  `gorm:"column:updated_at;autoUpdateTime" json:"updated_at"`
+	ID        string `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
+	UserID    int64  `gorm:"column:user_id;uniqueIndex" json:"user_id"`
+	Username  string `gorm:"column:username" json:"username"`
+	FarmID    int64  `gorm:"column:farm_id" json:"farm_id"`
+	LandID    int64  `gorm:"column:land_id" json:"land_id"`
+	Coin      int64  `gorm:"column:coin;default:0" json:"coin"`
+	Status    int64  `gorm:"column:status;default:1" json:"status"`
+	UpdatedAt int64  `gorm:"column:updated_at;autoUpdateTime" json:"updated_at"`
+	Sign      int8   `gorm:"column:sign;comment:'标记字段：1 表示新增行，-1 表示删除行'"` // 标记字段
+	Version   int64  `gorm:"column:version;comment:'版本号(Unix时间戳，微秒)'"`    // 版本号
 }
 
 // TableName 指定表名

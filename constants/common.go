6bf16c95c28036a216ef5a4637b1b0bcdc5eb9f6
o1was <guanlinhai520@gmail.com>
2025-04-22 18:22:10 +0800

    ✨ Feat(time): time
    time

--- constants/common.go
+++ constants/common.go
@@ -13,3 +13,17 @@ const (
 	EndpointAndroidApp          // Android_APP
 	EndpointIOSApp              // IOS_APP
 )
+
+// TimeSearchType 0-全部，1-今日，2-昨日，3-本周，4-上周，5-本月，6-上月，7-自定义
+type TimeSearchType int
+
+const (
+	TimeSearchTypeAll       TimeSearchType = iota // ALL
+	TimeSearchTypeToday                           // 今天
+	TimeSearchTypeYesterday                       // 昨天
+	TimeSearchTypeThisWeek                        // 本周
+	TimeSearchTypeLastWeek                        // 上周
+	TimeSearchTypeThisMonth                       // 本月
+	TimeSearchTypeLastMonth                       // 上月
+	TimeSearchTypeCustom                          // 自定义
+)

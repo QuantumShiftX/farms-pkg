f070330138f79367ac68956cdf0a7099343ff120
o1was <guanlinhai520@gmail.com>
2025-05-10 10:01:40 +0800

    ✨ Feat(☁️): ☁️
    ☁️

--- pmodel/payment_configs.gen.go
+++ pmodel/payment_configs.gen.go
@@ -13,7 +13,7 @@ type PaymentConfig struct {
 	CurrencyCode       string `gorm:"column:currency_code;type:varchar(255);not null" json:"currency_code"` // 货币代码，如"USDT"、"CNY"等
 	LogoURL            string `gorm:"column:logo_url;type:varchar(500)" json:"logo_url"`                    // LOGO图片URL地址，用于前端显示
 	HasExchangeRate    int64  `gorm:"column:has_exchange_rate;not null;default:2" json:"has_exchange_rate"` // 是否有汇率: 1:有, 2:无
-	ExchangeRate       int64  `gorm:"column:exchange_rate;not null;default:0" json:"exchange_rate"`         // 汇率值，表示1USDT兑换目标货币的数量
+	ExchangeRate       int64  `gorm:"column:exchange_rate;not null;default:0" json:"exchange_rate"`         // 汇率值，1 USDT 兑换目标货币的汇率 1usdt兑换0.998美元 那么exchange_rate=0.998*1000000
 	FixedFee           int64  `gorm:"column:fixed_fee;not null;default:0" json:"fixed_fee"`                 // 固定手续费金额，每笔交易收取的固定金额
 	PercentageFee      int64  `gorm:"column:percentage_fee;not null;default:0" json:"percentage_fee"`       //
 	Status             int64  `gorm:"column:status;not null;default:1" json:"status"`                       // 状态: 1:启用, 2:禁用

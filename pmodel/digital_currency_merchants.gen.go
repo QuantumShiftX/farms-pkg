ce4e425e286c7862879323ea6af7c9123077ba73
o1was <o1was@gmail.com>
2025-06-21 10:49:52 +0800

    ✨ Feat(dd): dd
    dd

--- pmodel/digital_currency_merchants.gen.go
+++ pmodel/digital_currency_merchants.gen.go
@@ -10,10 +10,11 @@ type DigitalCurrencyMerchant struct {
 	Name                string `gorm:"column:name;type:varchar(500);not null" json:"name"`                                     // 币商名称
 	Logo                string `gorm:"column:logo;type:varchar(500);not null" json:"logo"`                                     // 币商LOGO地址
 	Protocol            string `gorm:"column:protocol;type:varchar(500);not null" json:"protocol"`                             // 币商协议
-	ExchangeRate        int64  `gorm:"column:exchange_rate;type:int8;not null" json:"exchange_rate"`                           // 汇率(代币兑换USDT)固定1000个代币兑换多少ustd的汇率
-	FloatingRate        int64  `gorm:"column:floating_rate;type:int8;not null;default:0" json:"floating_rate"`                 // 浮动汇率(USDT)
+	ExchangeRate        int64  `gorm:"column:exchange_rate;type:int8;not null" json:"exchange_rate"`                           // 基准汇率(代币兑换USDT)类似1个代币兑换多少ustd的汇率
+	UpwardFluctuation   int64  `gorm:"column:upward_fluctuation;type:int8;not null;default:0" json:"upward_fluctuation"`       // 上浮幅度（正向波动范围）
+	DownwardFluctuation int64  `gorm:"column:downward_fluctuation;type:int8;not null;default:0" json:"downward_fluctuation"`   // 下浮幅度（负向波动范围）
 	MinCollectionAmount int64  `gorm:"column:min_collection_amount;type:int8;not null;default:0" json:"min_collection_amount"` // 最小收购金额(代币)
-	MaxCollectionAmount int64  `gorm:"column:max_collection_amount;type:int8;not null;default:0" json:"max_collection_amount"` // 最大收购金额(代币)';
+	MaxCollectionAmount int64  `gorm:"column:max_collection_amount;type:int8;not null;default:0" json:"max_collection_amount"` // 最大收购金额(代币)
 	PurchaseAmount      int64  `gorm:"column:purchase_amount;type:int8;not null;default:0" json:"purchase_amount"`             // 已收购金额(代币)
 	OrderAmount         int64  `gorm:"column:order_amount;type:int8;not null;default:0" json:"order_amount"`                   // 已订购金额(USDT)
 	Balance             int64  `gorm:"column:balance;type:int8;not null;default:0" json:"balance"`                             // 库存余额(USDT)
